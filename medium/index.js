28ffe4c9e3a39ab35102078a582991d079dbccbd
mori-c <moriconstant@gmail.com>
2019-06-17 15:02:22 -0400

    update index.js url

--- medium/index.js
+++ medium/index.js
@@ -1,25 +1,12 @@
-// medium_publisher
-var medium = require( 'medium-sdk' )
+// index.js
+// ...
+app.post( '/gh-deploy-server-receive-webhook-api-callback', function ( req, res ) {
+  var result = receiver.inspect( req.body )
 
-module.exports = {
-  publish: function ( payload ) {
-    var client = new medium.MediumClient( {
-      clientId: process.env.CLIENT_ID,
-      clientSecret: process.env.CLIENT_SECRET
-    } )
-    client.setAccessToken( process.env.ACCESS_TOKEN )
-    client.getUser( function ( err, user ) {
-      client.createPost( {
-        userId: user.id,
-        title: payload.title,
-        contentFormat: medium.PostContentFormat.MARKDOWN,
-        content: payload.body,
-        publishStatus: medium.PostPublishStatus.DRAFT
-      }, function ( err, post ) {
-        if ( err ) {
-          console.log( err )
-        }
-      } )
-    } )
+  if ( result.shouldPublish ) {
+    publisher.publish( result.payload )
   }
-}
+
+  res.send( 'callback!' )
+} )
+// ...
